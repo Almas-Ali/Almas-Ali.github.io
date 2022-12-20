2c4f2bf09f387fe1c0720619d65b1ff3ce050b39
Md. Almas Ali <almaspr3@gmail.com>
2022-12-21 02:19:26 +0600

    updated for deploy

--- react-portfolio/src/components/Index.js
+++ react-portfolio/src/components/Index.js
@@ -1,15 +1,7 @@
 import author from '../img/author.png';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 export default function Index() {
-
-  useEffect(() => {
-    let value = document.getElementsByClassName('persent');
-    for (let index = 0; index < value.length; index++) {
-      // const element = value[index];
-      value[index].style.width = value[index].innerText;
-    }
-  }, []);
 
   return (
     <>
@@ -26,206 +18,19 @@ export default function Index() {
           </p>
         </div>
 
-        <div className="connections" id="skills">
-          <p className="title skills">Skills</p>
-          <ul className="programming_languages">
-            <p className="title">Pogramming Languages</p>
 
-            <div className="pro_level">
-              <i className="fab fa-python" title="Python"></i>
-              <div className="progressbar">
-                <div className="persent">95%</div>
-              </div>
-            </div>
+        <div className="container">
 
-            <div className="pro_level">
-              <i className="fa-solid fa-c" title="C"></i>
-              <div className="progressbar">
-                <div className="persent">30%</div>
-              </div>
-            </div>
+          <div className="details">
+            <details>
+              <summary>Works
+              </summary>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error aliquid totam esse sed minima, aut in delectus porro vitae nihil itaque obcaecati praesentium nostrum, laboriosam voluptatibus deleniti laborum sapiente, alias consectetur iusto fuga illum provident. Cum officia corrupti recusandae iusto aperiam aut consequuntur sint atque illo cupiditate eum debitis consectetur possimus quidem, nobis dolorum optio. Cum cupiditate eligendi aliquam alias atque inventore culpa libero animi, repellendus quo repudiandae sunt eius eaque consequuntur reiciendis dolores esse odio sit. Officiis laboriosam impedit nemo earum saepe molestias voluptatum, aliquid veniam quo fuga alias labore itaque aspernatur assumenda sapiente quaerat accusantium consequuntur id facilis ipsa sit! Nobis tenetur iste consectetur sed animi ea ab adipisci repellendus placeat vel, similique, nemo incidunt, ex officia excepturi quam quaerat modi. Repellat, beatae temporibus? Quam consequatur maiores explicabo dolore quia tenetur nisi, exercitationem excepturi? Odit officiis maxime perferendis ullam, dignissimos illo? Explicabo voluptatum laudantium animi illo! Quisquam doloribus laboriosam obcaecati, odit hic corrupti sunt dignissimos nihil. A nisi minus deleniti delectus dolorum pariatur architecto modi, eos, repellendus error voluptas ipsum dignissimos alias eum incidunt animi. In quidem possimus doloremque totam ullam dolores dignissimos sunt. Nesciunt, sed sapiente! Soluta repellat delectus fugit placeat. Cumque ipsa sit iusto excepturi labore necessitatibus esse vel sapiente. Deserunt incidunt repudiandae aspernatur dicta eaque ut tempore, iusto aut et, corrupti illum sed accusantium asperiores nihil architecto quaerat assumenda! Voluptas magni quibusdam ex laborum perspiciatis reiciendis unde at ab, mollitia, repudiandae optio veritatis placeat! Qui quaerat aspernatur quam dolorem unde tempora quos labore aut fugiat voluptate. Placeat a, repudiandae consequatur consequuntur aperiam quaerat quos repellendus magni id, numquam adipisci qui, officia cum assumenda sapiente nesciunt. Amet iusto consequatur, impedit quaerat voluptatem id, velit, ratione accusamus mollitia maxime corporis doloribus hic vero unde! Incidunt voluptatem possimus quaerat itaque necessitatibus nulla quasi exercitationem laborum porro eligendi, fugiat libero officia tempore quia repellendus sequi? Consequatur assumenda, ducimus corrupti sit blanditiis, reprehenderit incidunt pariatur illum aliquam recusandae natus error quas dignissimos laudantium corporis rem? Illo inventore in, neque, commodi harum sequi esse dolore amet mollitia doloremque alias illum, dolores eum. Illo dolorum consequuntur obcaecati?
+            </details>
+          </div>
 
-          </ul>
-
-          <ul className="frontend">
-            <p className="title">Frontend Languages</p>
-
-            <div className="pro_level">
-              <i className="fab fa-html5" aria-hidden="true" title="HTML5"></i>
-              <div className="progressbar">
-                <div className="persent">90%</div>
-              </div>
-            </div>
-
-            <div className="pro_level">
-              <i className="fab fa-css3-alt" aria-hidden="true" title="CSS3"></i>
-              <div className="progressbar">
-                <div className="persent">60%</div>
-              </div>
-            </div>
-
-            <div className="pro_level">
-              <i className="fab fa-js" title="Javascript"></i>
-              <div className="progressbar">
-                <div className="persent">20%</div>
-              </div>
-            </div>
-          </ul>
-
-          <ul className="frontend_framework">
-            <p className="title">Frontend Framework</p>
-
-            <div className="pro_level">
-              <i className="fa-brands fa-bootstrap" title="Bootstrap"></i>
-              <div className="progressbar">
-                <div className="persent">60%</div>
-              </div>
-            </div>
-
-            <div className="pro_level">
-              <i className="fa-brands fa-react" title="ReactJs"></i>
-              <div className="progressbar">
-                <div className="persent">15%</div>
-              </div>
-            </div>
-
-          </ul>
-
-          <ul className="backend_languages">
-            <p className="title">Backend Languages</p>
-
-            <div className="pro_level">
-              <i className="fab fa-php" title="PHP"></i>
-              <div className="progressbar">
-                <div className="persent">25%</div>
-              </div>
-            </div>
-
-          </ul>
-
-          <ul className="backend_framework">
-            <p className="title">Backend Framework</p>
-
-            <div className="pro_level">
-              <i className="fa-brands fa-laravel" title="Laravel"></i>
-              <div className="progressbar">
-                <div className="persent">75%</div>
-              </div>
-            </div>
-            <div className="pro_level">
-              django
-              <div className="progressbar">
-                <div className="persent">65%</div>
-              </div>
-            </div>
-            <div className="pro_level">
-              <i className="fab fa-affiliatetheme" title="Flask"></i>
-              <div className="progressbar">
-                <div className="persent">25%</div>
-              </div>
-            </div>
-
-          </ul>
-
-          <ul className="database">
-            <p className="title">Database</p>
-
-            <div className="pro_level">
-              <i className="fa fa-database" aria-hidden="true" title="MySQL"></i>
-              <div className="progressbar">
-                <div className="persent">20%</div>
-              </div>
-            </div>
-
-          </ul>
-
-          <ul className="scripting_languages">
-            <p className="title">Scripting Languages</p>
-
-            <div className="pro_level">
-              <i className="fa fa-cube" aria-hidden="true" title="shell"></i>
-              <div className="progressbar">
-                <div className="persent">80%</div>
-              </div>
-            </div>
-
-
-          </ul>
-
-          <ul className="version_management">
-            <p className="title">Version Management</p>
-
-            <div className="pro_level">
-              <i className="fab fa-git" title="Git"></i>
-              <div className="progressbar">
-                <div className="persent">30%</div>
-              </div>
-            </div>
-          </ul>
-
-          <ul className="browsers">
-            <p className="title">Browsers</p>
-
-            <div className="pro_level">
-              <i className="fab fa-firefox" aria-hidden="true" title="Firefox"></i>
-              <div className="progressbar">
-                <div className="persent">80%</div>
-              </div>
-            </div>
-
-            <div className="pro_level">
-              <i className="fab fa-chrome" aria-hidden="true" title="Chrome"></i>
-              <div className="progressbar">
-                <div className="persent">75%</div>
-              </div>
-            </div>
-
-            <div className="pro_level">
-              <i className="fab fa-opera" aria-hidden="true" title="Opera"></i>
-              <div className="progressbar">
-                <div className="persent">60%</div>
-              </div>
-            </div>
-
-          </ul>
-
-          <ul className="operating_systems">
-            <p className="title">Operating Systems</p>
-
-            <div className="pro_level">
-              <i className="fab fa-windows" aria-hidden="true" title="Windows"></i>
-              <div className="progressbar">
-                <div className="persent">95%</div>
-              </div>
-            </div>
-
-            <div className="pro_level">
-              <i className="fab fa-linux" aria-hidden="true" title="Linux"></i>
-              <div className="progressbar">
-                <div className="persent">80%</div>
-              </div>
-            </div>
-          </ul>
-
-          <ul className="social_media">
-            <p className="title">Social Media Links</p>
-
-            <i className="fab fa-github" aria-hidden="true" title="Github"></i>
-            <i className="fab fa-facebook" aria-hidden="true" title="Facebook"></i>
-            <i className="fab fa-facebook-messenger" aria-hidden="true" title="Messenger"></i>
-            <i className="fab fa-instagram" aria-hidden="true" title="Instagram"></i>
-
-          </ul>
         </div>
 
-        <div className="details">
-          <details>
-            <summary>Works
-            </summary>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, velit quae. Molestias nostrum pariatur
-            doloremque, sunt magnam impedit dolor omnis?
-          </details>
-        </div>
       </main>
     </>
   )
